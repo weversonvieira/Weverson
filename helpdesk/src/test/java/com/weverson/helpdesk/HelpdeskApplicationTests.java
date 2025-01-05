66867787e4b6c77996cba0e3af2c5e01e58d0fcf
Weverson <weverson.vieira@acpgroup.com.br>
2025-01-05 15:08:39 -0300

    Projeto Iniciado

---
+++ helpdesk/src/test/java/com/weverson/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weverson.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
