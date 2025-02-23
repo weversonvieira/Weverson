3ab1d8168f84cd1362edb9985e36570a991cc086
Weverson <weverson.vieira@acpgroup.com.br>
2025-02-22 23:20:00 -0300

    id #306617 - BUG - ajuste na consolidacao do regimes

---
+++ helpdesk/src/main/java/com/weverson/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.weverson.helpdesk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+	
+
+}
